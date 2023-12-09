100024bc465c8f66d6427e5d6485b3d0c8916aa0
Rui <47530765+ruipo@users.noreply.github.com>
2023-12-09 16:44:21 -0500

    added stats bar

--- js/scripts.js
+++ js/scripts.js
@@ -10720,7 +10720,43 @@ Main Js
 
 });
 
+$(function () {
+    var fx = function fx() {
+    $(".stat-number").each(function (i, el) {
+        var data = parseInt(this.dataset.n, 10);
+        var props = {
+            "from": {
+                "count": 0
+            },
+                "to": {
+                "count": data
+            }
+        };
+        $(props.from).animate(props.to, {
+            duration: 1000 * 1,
+            step: function (now, fx) {
+                $(el).text(Math.ceil(now));
+            },
+            complete:function() {
+                if (el.dataset.sym !== undefined) {
+                  el.textContent = el.textContent.concat(el.dataset.sym)
+                }
+            }
+        });
+    });
+    };
 
+   /* var reset = function reset() {
+        //console.log($(this).scrollTop())
+        if ($(this).scrollTop()) {
+            $(this).off("scroll");
+          fx()
+        }
+    };*/
+
+    window.onload = fx();
+    //$(window).on("scroll", fx);
+});
  
 
 
